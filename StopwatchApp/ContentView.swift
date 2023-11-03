431db2d00adac9f8ee783ce83ffd4b699310da6e
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2023-11-03 09:46:18 -0400

    Initial Commit

---
+++ StopwatchApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StopwatchApp
+//
+//  Created by 宮川義之助 on 2023/11/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StopwatchApp/StopwatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchAppApp.swift
+//  StopwatchApp
+//
+//  Created by 宮川義之助 on 2023/11/03.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
