84f4c3c9b772a9ce1ad7b55fb98b21a7c29c5bc6
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2023-11-06 09:10:11 -0500

    Got the basic structure of the first circular button completed.

--- StopwatchApp/ContentView.swift
+++ StopwatchApp/ContentView.swift
@@ -11,15 +11,35 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             
-            //First layer (background)
+            // First layer (background)
             Color.black
                 .ignoresSafeArea()
             
-            //Secong layer (rest of interface)
+            // Secong layer (rest of interface)
             VStack {
                 Text("00:09.96")
                     .font(Font.system(size: 90, weight: .thin))
                     .foregroundColor(.white)
+                
+                // Create a circular button
+                ZStack {
+                    // First layer
+                    Circle()
+                        .foregroundColor(.gray)
+                    .frame(width: 100)
+                    // Second layer
+                    Circle()
+                        .foregroundColor(.black)
+                    .frame(width: 93)
+                    // Third layer
+                    Circle()
+                        .foregroundColor(.gray)
+                    .frame(width: 89)
+                    // Fourth layer
+                    Text("Reset")
+                        .font(.title2)
+                        .foregroundColor(.white)
+                }
             }
             .padding()
         }
