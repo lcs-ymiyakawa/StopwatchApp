a179b8869941b3ca0f8bcbfbeb18bdf65e7bdf1a
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2023-11-07 14:45:29 -0500

    Structured a code for Lap time

---
+++ StopwatchApp/TimeListView.swift
@@ -0,0 +1,18 @@
+//
+//  TimeListView.swift
+//  StopwatchApp
+//
+//  Created by 宮川義之助 on 2023/11/06.
+//
+
+import SwiftUI
+
+struct TimeListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimeListView()
+}
